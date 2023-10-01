a13db5406d6778fb6bba70cf5e716b4803b4334a
AxelJaBo <axel.910921@gmail.com>
2023-09-30 19:56:33 -0700

    Decoradores

--- src/app/services/product-crud.service.ts
+++ src/app/services/product-crud.service.ts
@@ -1,16 +1,15 @@
 import { UpdateProductDto } from "../dtos/product.dto";
 import { Product } from "../models/product.model";
-import { BaseHttpService } from "./base-http.service";
 import { ProductHttpService } from "./product-http2.service";
 
 export class ProductCRUDService {
   private url = 'https://api.escuelajs.co/api/v1/products';
-  private http = new BaseHttpService(this.url);
+  private http = new ProductHttpService(this.url);
 
   async update(id: Product['id'], dto: UpdateProductDto){
     // permisos
     // logica
-    this.http.otroRequest
+    this.http.otroRequest;
     return this.http.update(id, dto);
   }
 }
