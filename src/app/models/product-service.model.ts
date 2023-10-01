ea6dafdb1aafe848e4911e338649036475ca0fb7
AxelJaBo <axel.910921@gmail.com>
2023-09-30 18:19:03 -0700

    ProductHttpService

---
+++ src/app/models/product-service.model.ts
@@ -0,0 +1,9 @@
+import { CreateProductDto, UpdateProductDto } from "../dtos/product.dto";
+import { Product } from "./product.model";
+
+export interface ProductService {
+  create(dto: CreateProductDto): Product | Promise<Product[]>;
+  getAll(): Product[] | Promise<Product[]>;
+  findOne(id: Product['id']): Product | undefined | Promise<Product | undefined>;
+  update(id: Product['id'], changes: UpdateProductDto): Product | Promise<Product>;
+}
